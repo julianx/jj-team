c264bfc1f21b3ffebc5bcaada33be13a8af83462
Jimmy Garagorry <jgaragorry@HW13172.local>
2018-08-08 13:58:57 -0400

    Check for all data nodes folder permissions in a cluster

---
+++ check_dn_dir_perms.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+for Nodes in $(su -l hdfs -c "hdfs dfsadmin -report" | awk '/^Hostname:/ {print $NF}')
+do
+  echo ${Nodes}
+  for DataNode in $(ssh root@${Nodes} "grep -C 2 'dfs.datanode.data.dir<' /etc/hadoop/conf/hdfs-site.xml | awk -F '[<|>]' '/value/ {print \$3}'")
+  do
+   namei -l ${DataNode}
+   echo
+  done
+done
