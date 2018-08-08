821dff2a617328904676fbb5850f3eef81c0f62f
Jimmy Garagorry <jgaragorry@HW13172.local>
2018-08-08 14:40:24 -0400

    Fixed argument list for data dir

--- check_dn_dir_perms.sh
+++ check_dn_dir_perms.sh
@@ -3,7 +3,7 @@
 for Nodes in $(su -l hdfs -c "hdfs dfsadmin -report" | awk '/^Hostname:/ {print $NF}')
 do
   echo ${Nodes}
-  for DataNode in $(ssh root@${Nodes} "grep -C 2 'dfs.datanode.data.dir<' /etc/hadoop/conf/hdfs-site.xml | awk -F '[<|>]' '/value/ {print \$3}'")
+  for DataNode in $(ssh root@${Nodes} "grep -C 2 'dfs.datanode.data.dir<' /etc/hadoop/conf/hdfs-site.xml | awk -F '[<|>]' '/value/ {print \$3}' | sed 's|,| |g'")
   do
    namei -l ${DataNode}
    echo
